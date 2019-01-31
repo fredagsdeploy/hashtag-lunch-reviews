3e815ea3ac765a2516b44edb22039951a8c23ad5
André Samuelsson <andresamuelsson94@gmail.com>
2019-01-31 10:53:27 +0100

    Remove order functions in favour of lodash's orderBy

--- src/components/StatsController.tsx
+++ src/components/StatsController.tsx
@@ -19,7 +19,7 @@ export default () => {
       .then(load)
       .then((ratings: Array<Rating>) => {
         console.log("Loaded data", ratings);
-        setRatings(ratings.sort(byRating));
+        setRatings(_.orderBy(ratings, [sortedBy], "desc"));
       })
       .catch((err: Error) => {
         console.log("Error on loading data", err);
@@ -28,47 +28,14 @@ export default () => {
 
   useEffect(() => {
     window.gapi.load("client", initClient);
-    return function() {};
   }, []);
-
-  const compareString = (s1: String, s2: String) => {
-    if (s1 === s2) {
-      return 0;
-    }
-    if (s1 < s2) {
-      return -1;
-    }
-    return 1;
-  };
-
-  const byName = (r1: Rating, r2: Rating): number =>
-    compareString(r1.name, r2.name);
-  const byRating = (r1: Rating, r2: Rating): number => r1.rating - r2.rating;
-  const byComment = (r1: Rating, r2: Rating): number =>
-    compareString(r1.comment, r2.comment);
-
-  const selectSortFunction = (
-    headerKey: any
-  ): ((r1: Rating, r2: Rating) => number) => {
-    type Dictionary = { [index: string]: ((r1: Rating, r2: Rating) => number) };
-    const functions: Dictionary = {
-      name: byName,
-      rating: byRating,
-      comment: byComment
-    };
-    if (headerKey in functions) {
-      return functions[headerKey];
-    }
-    throw new Error(`no function found for header ${headerKey}`);
-  };
 
   const sortBy = (headerKey: string) => {
     if (sortedBy === headerKey) {
       setRatings(ratings.reverse());
       return;
     }
-    const sortFunction = selectSortFunction(headerKey);
-    const sortedRatings = ratings.sort(sortFunction);
+    const sortedRatings = _.orderBy(ratings, [headerKey]);
     setSortedBy(headerKey);
     setRatings(sortedRatings);
   };
