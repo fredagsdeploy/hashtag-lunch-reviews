98c7c3151c3d63cfc6291993ad3fd028f8e71d4f
André Samuelsson <andresamuelsson94@gmail.com>
2019-03-26 19:35:07 +0100

    Fix deployment to aws lambda process.

--- serverless-api/src/repository/documentClient.ts
+++ serverless-api/src/repository/documentClient.ts
@@ -1,8 +1,17 @@
 import AWS = require("aws-sdk");
 
-export const dynamodb = new AWS.DynamoDB.DocumentClient({
-  region: "localhost",
-  endpoint: process.env.DYNAMODB_URL,
-  accessKeyId: "DEFAULT_ACCESS_KEY", // needed if you don't have aws credentials at all in env
-  secretAccessKey: "DEFAULT_SECRET" // needed if you don't have aws credentials at all in env
-});
+export let dynamodb: AWS.DynamoDB.DocumentClient;
+if (process.env.STAGE_ENV === "dev") {
+  dynamodb = new AWS.DynamoDB.DocumentClient({
+    region: "localhost",
+    endpoint: process.env.DYNAMODB_URL
+  });
+} else if (process.env.STAGE_ENV === "prod") {
+  AWS.config.update({ region: "eu-north-1" });
+
+  dynamodb = new AWS.DynamoDB.DocumentClient({
+    apiVersion: "2012-08-10"
+  });
+} else {
+  throw new Error("Invalid STAGE_ENV");
+}
