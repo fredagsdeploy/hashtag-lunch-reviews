18e2018c913ff47a4bd3282e9ee6276490932e63
Joel Gustafsson <me@joelgustafsson.com>
2019-03-29 14:37:47 +0100

    Reduce sizes of stars

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -7,12 +7,13 @@ interface Props {
 }
 
 export const StarRating = (props: Props) => {
+  const starSize = 16;
   const rating = _.min([5, props.rating]) || 0;
 
   return (
     <>
-      <StarDiv style={{ width: rating * 28 }} />
-      <HollowStarDiv style={{ width: 5 * 28 }} />
+      <StarDiv style={{ width: rating * starSize }} />
+      <HollowStarDiv style={{ width: 5 * starSize }} />
     </>
   );
 };
@@ -20,14 +21,16 @@ export const StarRating = (props: Props) => {
 const StarDiv = styled.div`
   background-image: url(/starsolid.svg);
   background-repeat: repeat-x;
-  height: 28px;
+  background-size: 16px;
+  height: 16px;
   position: absolute;
   z-index: 1;
 `;
 
 const HollowStarDiv = styled.div`
   background-image: url(/starhollow.svg);
+  background-size: 16px;
   background-repeat: repeat-x;
-  height: 28px;
+  height: 16px;
   z-index: 0;
 `;
