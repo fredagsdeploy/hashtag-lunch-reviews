7a7a9b30400d19dcf46b9221727dc0558ebd440d
André Samuelsson <andresamuelsson94@gmail.com>
2019-04-17 23:01:30 +0200

    Cleanup ratings code

--- serverless-api/src/ratings.ts
+++ serverless-api/src/ratings.ts
@@ -1,49 +1,53 @@
-import { getAllReviews } from "./repository/reviews";
+import { getAllReviews, getReviewsByPlaceId } from "./repository/reviews";
 import * as _ from "lodash";
 import { createResponse, LambdaHandler } from "./common";
-import { getAllPlaces } from "./repository/places";
+import { getAllPlaces, getPlaceByGoogleId, getPlaceById } from "./repository/places";
 import { getGooglePlace } from "./googlePlaces/googlePlaces";
+import { getReviewsByPlace } from "./reviews";
 
 interface Rating {
   placeId: string;
   rating: number;
 }
 
+const calculateRating = (place, reviews) => {
+  if (!_.isEmpty(reviews)) {
+    const sum = reviews.reduce((sum, review) => review.rating + sum, 0);
+
+    return sum / reviews.length
+  }
+
+  return 0;
+}
+
+const getGooglePlaceInfo = async (place) => {
+  if (place.googlePlaceId) {
+    const googlePlace = await getGooglePlace(place.googlePlaceId).then(
+      r => r.json()
+    );
+    return googlePlace.result
+  }
+}
+
+const decoratePlace = async (place, reviews) => {
+  const rating = calculateRating(place, reviews)
+  const googlePlace = await getGooglePlaceInfo(place);
+
+  return {
+    ...place,
+    rating,
+    googlePlace
+  }
+}
+
 export const getPlacesWithRatings: LambdaHandler = async () => {
   const reviews = await getAllReviews();
-
   const places = await getAllPlaces();
 
   const reviewsByPlace = _.groupBy(reviews, r => r.placeId);
 
   const placesWithRatings = await Promise.all(
-    _.map(places, async place => {
-      const placeId = place.placeId;
-      if (placeId in reviewsByPlace) {
-        const reviews = reviewsByPlace[placeId];
-        const sum = reviews.reduce((sum, review) => review.rating + sum, 0);
-
-        const rating = {
-          ...place,
-          rating: sum / reviews.length
-        };
-
-        if (place.googlePlaceId) {
-          const googlePlace = await getGooglePlace(place.googlePlaceId).then(
-            r => r.json()
-          );
-
-          return {
-            ...rating,
-            googlePlace: googlePlace.result
-          };
-        } else {
-          return rating;
-        }
-      }
-
-      return { ...place, rating: 0 };
-    })
+    _.map(places, place => decoratePlace(place, reviewsByPlace[place.placeId]))
   );
 
   const rankedRatings = _.orderBy(placesWithRatings, "rating", "desc").map(
